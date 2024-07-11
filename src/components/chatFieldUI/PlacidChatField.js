efe0b53f9cee9005210b8d5f4ac588c3f3b7afe4
AlexProgrammer1234 <147748792+AlexProgrammer1234@users.noreply.github.com>
2024-07-11 23:28:55 +0300

    Fixed bugs

--- src/components/chatFieldUI/PlacidChatField.js
+++ src/components/chatFieldUI/PlacidChatField.js
@@ -2,6 +2,7 @@ import { Paper, useMediaQuery } from "@mui/material";
 import Message from "../messageUI/Message";
 import InputField from "../inputFieldUI/InputField";
 import { useState } from "react";
+import "./PlacidChatField.css";
 
 export default function PlacidChatField({ user }) {
   const [messages, setMessages] = useState([]);
@@ -11,13 +12,14 @@ export default function PlacidChatField({ user }) {
     <Paper
       elevation={0}
       sx={{
-        maxHeight: "100vh",
         display: "flex",
         flexDirection: "column",
         background: "none",
         position: "relative",
         zIndex: "1",
         marginTop: "60px",
+        boxSizing: "border-box",
+        height: "calc(100vh - 60px)",
       }}
     >
       <Paper
@@ -28,14 +30,15 @@ export default function PlacidChatField({ user }) {
           border: "1px solid rgba(255, 255, 255, 0.1)",
           padding: "10px",
           overflow: "auto",
-          paddingBottom: "105px",
-          height: "100%",
+          paddingBottom: "80px",
           display: "flex",
           flexDirection: "column",
+          height: "100%",
           alignItems: "end",
           paddingLeft: isSmallScreen ? "10px" : "23vw",
           marginLeft: isSmallScreen ? "80px" : "5px",
         }}
+        className="messageField"
       >
         {messages?.map((e, index) => {
           return (
